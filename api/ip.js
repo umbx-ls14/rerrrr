0f41f436934e510887ad3da341e56770e5f632e9
umbx-ls14 <leoschlegel11@gmail.com>
2025-05-05 18:26:14 +0200

    ip.js

--- api/ip.js
+++ api/ip.js
@@ -1,5 +1,10 @@
 export default function handler(req, res) {
-    res.setHeader("Content-Type", "text/plain");
-    res.status(200).send("127.0.0.1");
+  const key = req.query.key || req.headers.key;
+  if (key !== "2504") {
+    res.status(404).end();
+    return;
   }
-  
+
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send("1.1.1.1");
+}
